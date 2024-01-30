c13bf4dfafe7a47e12cfcac19fde1bb8af898eb5
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-01-30 10:31:56 -0300

    video1.3

---
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -0,0 +1,19 @@
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test;
+
+public class OfertaViagemTest
+{
+    [Fact]
+    public void TestandoOfertaValida()
+    {
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = 100.0;
+        var validacao = true;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        Assert.Equal(validacao, oferta.EhValido);
+    }
+}
