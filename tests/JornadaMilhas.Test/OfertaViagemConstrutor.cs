20b53f2251ff5092e05c3dd7ff4f089e424ae0b0
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-01 10:57:18 -0300

    video 2.3

--- tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -4,14 +4,17 @@ namespace JornadaMilhas.Test;
 
 public class OfertaViagemConstrutor
 {
-    [Fact]
-    public void RetornaOfertaValidaQuandoDadosValidos()
+    [Theory]
+    [InlineData("", null, "2024-01-01", "2024-01-02", 0, false)]
+    [InlineData("OrigemTeste", "DestinoTeste", "2024-02-01", "2024-02-05", 100, true)]
+    [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+    [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+    [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+    public void RetornaEhValidoDeAcordoComDadosDeEntrada(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
     {
         //arrange
-        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
-        double preco = 100.0;
-        var validacao = true;
+        Rota rota = new Rota(origem, destino);
+        Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
 
         //act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
