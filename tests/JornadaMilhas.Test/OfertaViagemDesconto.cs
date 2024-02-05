e32a687cbe610f5cd90da504171313adc2dc8f2e
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-05 14:07:21 -0300

    video 4.3

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -26,15 +26,15 @@ public class OfertaViagemDesconto
         Assert.Equal(precoComDesconto, oferta.Preco);
     }
 
-    [Fact]
-    public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+    [Theory]
+    [InlineData(120, 30)]
+    [InlineData(100, 30)]
+    public void RetornaDescontoMaximoQuandoValorDescontoMaiorOuIgualAoPreco(double desconto, double precoComDesconto)
     {
         //arrange
         Rota rota = new Rota("OrigemA", "DestinoB");
         Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
         double precoOriginal = 100.00;
-        double desconto = 120.00;
-        double precoComDesconto = 30.00;
         OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
 
         //act
