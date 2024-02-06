8c94399bbf40af69d5cee8dd364a0869cb6a1e59
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-06 11:51:39 -0300

    video 5.2

--- tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
+++ tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
@@ -1,4 +1,5 @@
-﻿using JornadaMilhasV1.Gerenciador;
+﻿using Bogus;
+using JornadaMilhasV1.Gerenciador;
 using JornadaMilhasV1.Modelos;
 using System;
 using System.Collections.Generic;
@@ -29,7 +30,33 @@ public class GerenciadorDeOfertasRecuperaMaiorDesconto
     public void RetornaOfertaEspecificaQuandoDestinoSaoPauloEDesconto40()
     {
         //arrange
-        var lista = new List<OfertaViagem>();
+        var fakerPeriodo = new Faker<Periodo>()
+            .CustomInstantiator(f =>
+            {
+                DateTime dataInicio = f.Date.Soon();
+                return new Periodo(dataInicio, dataInicio.AddDays(30));
+            });
+
+        var rota = new Rota("Curitiba", "São Paulo");
+
+        var fakerOferta = new Faker<OfertaViagem>()
+            .CustomInstantiator(f => new OfertaViagem (
+                rota,
+                fakerPeriodo.Generate(),
+                100 * f.Random.Int(1, 100))
+            )
+            .RuleFor(o => o.Desconto, f => 40)
+            .RuleFor(o => o.Ativa, f => true);
+
+        var ofertaEscolhida = new OfertaViagem(rota, fakerPeriodo.Generate(), 80)
+        {
+            Desconto = 40,
+            Ativa = true
+        };
+
+
+        var lista = fakerOferta.Generate(200);
+        lista.Add(ofertaEscolhida);
         var gerenciador = new GerenciadorDeOfertas(lista);
         Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
         var precoEsperado = 40;
