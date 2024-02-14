5053f93617faebbff2effcf924703c97f3162955
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-13 22:31:17 -0300

    mão na massa aula 01

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -35,4 +35,20 @@ public class OfertaViagemTest
         Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
     }
+
+    [Fact]
+    public void TestandoOfertaComPeriodoInvalido()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 5), new DateTime(2024, 2, 1));
+        double preco = 100.0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
 }
